992dc5d3735739f4e9fa43040f351244fe6914cb
Quinito Luís <qluis@estudantes.unisced.edu.mz>
2025-08-10 22:38:39 +0200

    Add files via upload
    Carregando todos os arquivos necesario para o jogo

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'jogo-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/acerto.mp3',
+  '/erro.mp3',
+  '/vitoria.mp3',
+  // Adicione aqui outros arquivos necessários, como CSS, JS, etc.
+];
+
+// Instala o service worker e faz o cache dos arquivos listados
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Ativa o service worker e limpa caches antigos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Intercepta as requisições e responde com cache ou fetch na rede
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
